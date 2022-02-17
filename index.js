436bad79837b47b590b60b435bf215cecab1b83b
Martín <magadiflo@gmail.com>
2022-02-17 10:51:22 -0500

    Creando las rutas de los servicios del usuario

--- index.js
+++ index.js
@@ -15,12 +15,9 @@ app.use(cors());
 dbConnection();
 
 //Rutas
-app.get('/', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Hola mundo',
-    });
-});
+//Cuando una petición vaya por esta ruta /api/usuarios/, será interceptada por el middleware de routes/usuarios.js
+app.use('/api/usuarios', require('./routes/usuarios'));
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
