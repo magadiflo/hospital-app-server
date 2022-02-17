abb7e6f7bb3586653e2fa34c7cf55bc750cb5253
Martín <magadiflo@gmail.com>
2022-02-16 22:51:54 -0500

    Iniciando nuestro servidor de Express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+//Crear el servidor express
+const app = express();
+
+
+
+
+
+app.listen(3000, () => {
+    console.log(`Servidor corriendo en el puerto 3000`);
+});
