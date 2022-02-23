faea08b41f58c4e8844ce2c23bd3788f3358f92d
Martín <magadiflo@gmail.com>
2022-02-23 13:02:47 -0500

    Búsqueda general en todas las colecciones de forma simultánea

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -1,11 +1,23 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
+const Medico = require('../models/Medico');
+const Hospital = require('../models/Hospital');
 
-const getTodo = (req, res = response) => {
+const getTodo = async (req, res = response) => {
     const busqueda = req.params.busqueda;
+    const regExp = new RegExp(busqueda, 'i');
+
+    const [usuarios, medicos, hospitales] = await Promise.all([
+        Usuario.find({nombre: regExp}),
+        Medico.find({nombre: regExp}),
+        Hospital.find({nombre: regExp})
+    ]);
+
     res.json({
         ok: true,
-        busqueda,
-        msg: 'buscando a todos...'
+        usuarios,
+        medicos,
+        hospitales
     });
 }
 
