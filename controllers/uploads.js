298d3f08237cb831791958459285a55016e7b5bd
Martín <magadiflo@gmail.com>
2022-02-24 11:22:44 -0500

    Subir archivos al servidor

---
+++ controllers/uploads.js
@@ -0,0 +1,35 @@
+const { response, json } = require('express');
+
+const fileUpload = (req, res = response) => {
+    const tipo = req.params.tipo;
+    const id = req.params.id;
+
+    //Validar tipo
+    const tiposValidos = ['hospitales', 'medicos', 'usuarios'];
+    if(!tiposValidos.includes(tipo)){
+        return res.status(400).json({
+            ok: false,
+            msg: 'No es un médico, usuario u hospital'
+        });
+    }
+
+    //Validar que exista un archivo
+    if(!req.files || Object.keys(req.files).length === 0){
+        return res.status(400).json({
+            ok: false,
+            msg: 'No hay ningún archivo',
+        });
+    }
+
+    // Procesar la imagen
+
+
+    res.json({
+        ok: true,
+        msg: 'fileUploaded'
+    });
+}
+
+module.exports = {
+    fileUpload,
+}
---
+++ routes/uploads.js
@@ -0,0 +1,17 @@
+/**
+ * Hospitales
+ * ruta: /api/upload/
+ */
+const { Router } = require('express');
+const expressFileUpload = require('express-fileupload');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { fileUpload } = require('../controllers/uploads');
+
+
+const router = Router();
+router.use(expressFileUpload());
+
+router.put('/:tipo/:id', [validarJWT], fileUpload);
+
+
+module.exports = router;
