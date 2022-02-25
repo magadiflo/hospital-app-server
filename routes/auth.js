ed164317383af0dc76bc4467b00e7cf1e5f77ea2
Martín <magadiflo@gmail.com>
2022-02-25 11:30:54 -0500

    Continuación del proyecto - Backend Server - Renovar Token

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,9 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login, googleSignIn } = require('../controllers/auth');
+const { login, googleSignIn, renewToken } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 
 const router = Router();
@@ -21,6 +22,10 @@ router.post('/google',[
     check('token', 'El token de google es obligatorio').not().isEmpty(),
     validarCampos
 ], googleSignIn);
+
+router.get('/renew',[
+    validarJWT
+], renewToken);
 
 
 
