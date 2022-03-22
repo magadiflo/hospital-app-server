5740c20285a6927062455a36581fcabb5c2e7a30
Martín <magadiflo@gmail.com>
2022-03-22 09:51:54 -0500

    Validar el ADMIN_ROLE en nuestro backend

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
 const jwt = require('jsonwebtoken');
+const Usuario = require('../models/Usuario');
 
 const validarJWT = (req, res = response, next) => {
 
@@ -28,6 +29,36 @@ const validarJWT = (req, res = response, next) => {
     }
 }
 
+const validarADMIN_ROLE = async (req, res, next) => {
+    const uid = req.uid;
+    try {
+        const usuarioDB = await Usuario.findById(uid);
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Usuario no existe',
+            });
+        }
+
+        if (usuarioDB.role !== 'ADMIN_ROLE') {
+            return res.status(403).json({
+                ok: false,
+                msg: 'No tiene privilegios para hacer eso',
+            });
+        }
+
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+}
+
 module.exports = {
     validarJWT,
+    validarADMIN_ROLE,
 }
